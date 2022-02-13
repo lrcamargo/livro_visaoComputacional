48cdf4545964a89f2483e1e187e2ad7e963a1f53
Letícia <38875003+lrcamargo@users.noreply.github.com>
2022-02-13 18:34:12 -0300

    Create ex22.py

---
+++ cap05/ex22.py
@@ -0,0 +1,17 @@
+#pagina 94
+import cv2
+import numpy as np
+
+imagemOriginal = cv2.imread("sudoku.jpeg")
+
+pontosIniciais = np.float32([[189,87], [459,84], [192,373], [484,372]])
+pontosFinais = np.float32([[0,0],[500,0],[0,500],[500,500]])
+
+matriz = cv2.getPerspectiveTransform(pontosIniciais,pontosFinais)
+imagemModificada = cv2.warpPerspective(imagemOriginal,matriz,(500,500))
+
+cv2.imshow("ImagemOriginal", imagemOriginal)
+cv2.imshow("ImagemModificada", imagemModificada)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
