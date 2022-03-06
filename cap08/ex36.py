e9847f3c6a5b77610d0e0be3402833bb6983e981
Letícia <38875003+lrcamargo@users.noreply.github.com>
2022-03-06 03:40:31 -0300

    Create ex36.py

---
+++ cap08/ex36.py
@@ -0,0 +1,15 @@
+#página 145
+import cv2 
+import numpy as np 
+#adaptado para rodar no google colab
+from google.colab.patches import cv2_imshow
+
+imagemOriginal = cv2.imread("/content/rolamento.jpg", 0) 
+elementoEstruturante = cv2.getStructuringElement( cv2.MORPH_ELLIPSE, (5,5) ) 
+imagemProcessada = cv2.dilate( imagemOriginal, elementoEstruturante, iterations = 2 ) 
+
+cv2_imshow(imagemOriginal) 
+cv2_imshow(imagemProcessada) 
+
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
